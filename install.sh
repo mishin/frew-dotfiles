37558d54f9ab3faea0c4a4fbf0a23685c3b23cfe
Arthur Axel 'fREW' Schmidt <frioux@gmail.com>
2013-12-19 18:44:06 -0600

    WIP

--- install.sh
+++ install.sh
@@ -1,62 +1,7 @@
 #!/bin/zsh
 
-echo "running rc installer"
-trap "echo 'done installing'" USR1
-
-function link-file { __mkdir "${2:h}"; rm -rf "$2"; ln -s "$PWD/$1" "$2" }
-function copy-file { __mkdir "${2:h}"; rm -rf "$2"; cp "$PWD/$1" "$2" }
-
 perl install-cronjobs.pl
-link-file awesome ~/.config/awesome
-link-file dotjs ~/.js
-link-file ssh/config ~/.ssh/config
-link-file ssh/authorized_keys ~/.ssh/authorized_keys
-link-file terminator_config ~/.config/terminator/config
-copy-file keepassx_config.ini ~/.config/keepassx/config.ini
-link-file install.sh .git/hooks/post-checkout
-link-file install.sh .git/hooks/post-merge
-link-file xsession ~/.xinitrc
-link-file gpg.conf ~/.gnupg/gpg.conf
-link-file todo ~/.todo/config
-link-file ww-roller.pl/ww_roll.pl ~/bin/ww-roll.pl
-source ~/.smartcd/lib/core/arrays
-source ~/.smartcd/lib/core/varstash
-source ~/.smartcd/lib/core/smartcd
 cat smartcd-export | smartcd import
-
-# literal dotfiles
-for x in           \
-   adenosinerc.yml \
-   dbic.json       \
-   dzil            \
-   gitconfig       \
-   gtkrc-2.0       \
-   gtkrc.mine      \
-   irssi           \
-   jshintrc        \
-   msmtprc         \
-   mailcap         \
-   mutt            \
-   muttrc          \
-   notmuch-config  \
-   offlineimaprc   \
-   offlineimap.py  \
-   perltidyrc      \
-   signature       \
-   tkremindrc      \
-   tmux.conf       \
-   Xdefaults       \
-   xmodmap         \
-   xscreensaver    \
-   xsession        \
-   zsh             \
-   weechat         \
-   zshrc           \
-   zshenv          \
-   fruperl         \
-; do
-   link-file $x ~/.$x
-done
 
 # vim works differently on win32
 case $OSTYPE in
@@ -74,8 +19,3 @@ case $OSTYPE in
       link-file vim ~/.vim
    ;;
 esac
-
-# ensure submodules are checked out before linking to them
-git submodule update --init --quiet
-link-file zsh/cxregs-bash-tools/lib ~/.smartcd/lib
-pkill -usr1 zsh
